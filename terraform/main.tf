0a6eb987ef70123b76c98ea67c62024e33c3bc8a
JoseVitorDurante <josevitor_limma@hotmail.com>
2025-01-13 11:59:20 -0300

    formated

--- terraform/main.tf
+++ terraform/main.tf
@@ -15,7 +15,7 @@ module "api_gateway" {
   lb_service_name_msc_pcs_midia  = "msc-pcs-midia-service"
   lb_service_port_msc_pcs_midia  = 89
   vpc_id                         = data.aws_ssm_parameter.vpc_id.value
-  private_subnet_ids = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
+  private_subnet_ids             = split(",", data.aws_ssm_parameter.private_subnet_ids.value)
 }
 
 resource "aws_ssm_parameter" "api_gateway_endpoint" {
